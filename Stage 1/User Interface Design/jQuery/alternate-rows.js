ac15d046485f17f58fe04a8ac907582871770592
Harsh Rastogi <rastogiharsh04@gmail.com>
2022-02-23 19:53:03 +0530

    TypeFixed
    `#` was present for finding table element, but the HTML structure does not contain any element with "id=table". 
    I have also added little formatting making this code more readable;

--- Stage 1/User Interface Design/jQuery/alternate-rows.js
+++ Stage 1/User Interface Design/jQuery/alternate-rows.js
@@ -1,3 +1,7 @@
-
-$('#table').find('tr:even').css({'background-color':'lightpink'})
-.end().find('tr:odd').css({'background-color':'lightblue'}).end().find('caption').css({'background-color':'lightblue'});
+$('table').find('tr:even')
+        .css({'background-color':'lightpink'})
+        .end()
+        .find('tr:odd')
+        .css({'background-color':'lightblue'})
+        .end()
+        .find('caption').css({'background-color':'lightblue'});
